95fdd9abb273d33fd5ee932e64bfa490b9414f49
DylanCoon99 <dylcoon99@gmail.com>
2024-02-17 22:52:48 -0500

    Product class done

--- ebay_clone/src/main/java/org/example/Product.java
+++ ebay_clone/src/main/java/org/example/Product.java
@@ -1,4 +1,35 @@
 package org.example;
 
 public class Product {
+
+    private String name;
+    private double price;
+    private String description;
+
+    // think about how to display an image for each product
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
 }
