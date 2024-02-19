cc0f2d4b4929466a62a159f9a5971520a934f2ad
DylanCoon99 <dylcoon99@gmail.com>
2024-02-18 22:00:29 -0500

    Transaction class done

--- ebay_clone/src/main/java/org/example/Transaction.java
+++ ebay_clone/src/main/java/org/example/Transaction.java
@@ -4,7 +4,30 @@ public class Transaction {
 
     private Integer buyerID;
     private Integer sellerID;
-    private String productID;
+    private Integer productID;
 
+    public void setBuyerID(Integer buyerID) {
+        this.buyerID = buyerID;
+    }
+
+    public Integer getBuyerID() {
+        return this.buyerID;
+    }
+
+    public void setSellerID(Integer sellerID) {
+        this.sellerID = sellerID;
+    }
+
+    public Integer getSellerID() {
+        return this.sellerID;
+    }
+
+    public void setProductID(Integer productID) {
+        this.productID = productID;
+    }
+
+    public Integer getProductID() {
+        return this.productID;
+    }
 
 }
