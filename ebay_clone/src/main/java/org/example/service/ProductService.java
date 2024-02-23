c2556a46c0915a176a05579285c565485a091fec
DylanCoon99 <dylcoon99@gmail.com>
2024-02-22 19:40:35 -0500

    transaction service done

--- ebay_clone/src/main/java/org/example/service/ProductService.java
+++ ebay_clone/src/main/java/org/example/service/ProductService.java
@@ -1,4 +1,44 @@
 package org.example.service;
 
+import org.example.Product;
+import org.example.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+    private final ProductRepo productRepo;
+
+    @Autowired
+    public ProductService(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Optional<Product> getProductById(int productId) {
+        Product product = productRepo.findById(productId);
+        return Optional.ofNullable(product);
+    }
+
+    public void createProduct(Product product) {
+        productRepo.save(product);
+    }
+
+    public void updateProduct(Product product) {
+        // Implement update logic
+        productRepo.update(product);
+    }
+
+    public void deleteProduct(Product product) {
+        productRepo.delete(product);
+    }
+
 }
